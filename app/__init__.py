5525b42db6a8bd42c2e3da8f0d08a91e0b6ac744
PluZ Kongsompong <geruby85@gmail.com>
2020-04-02 23:37:32 +0700

    Fixed Import loop

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import os
 from flask import Flask
 from flask_cors import CORS
 from app import route
 from app.api import api
 
-# configuration
-DEBUG = True
-
-# instantiate the app
+# Initialize application
 app = Flask(__name__)
 app.register_blueprint(api)
+
+# configuration
+app_settings = os.getenv(
+    'APP_SETTINGS',
+    'app.config.DevelopmentConfig'
+)
+app.config.from_object(app_settings)
+
+
 # Dev Config
 # app.config.from_object(__name__)
+DEBUG = True
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
