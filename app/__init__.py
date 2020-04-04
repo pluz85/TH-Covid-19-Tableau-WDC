bc6b12d3bbad8470ceda437ad895350e145e1c48
PluZ Kongsompong <geruby85@gmail.com>
2020-04-05 00:46:48 +0700

    Fix init process

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,13 @@
 from flask import Flask
 from flask_cors import CORS
 
+# Initialize application
+app = Flask(__name__)
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+app.config.from_object(__name__)
+with app.app_context():
+    from . import api, router
 
-def create_app():
-    # Initialize application
-    app = Flask(__name__)
-    # enable CORS
-    CORS(app, resources={r'/*': {'origins': '*'}})
-    # Dev Config
-    app.config.from_object(__name__)
-    with app.app_context():
-        from . import api, router
-        app.register_blueprint(api)
-        app.register_blueprint(router)
-    return app
-
-
-app = create_app()
+    app.register_blueprint(api)
+    app.register_blueprint(router)
